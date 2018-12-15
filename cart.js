dd150eefdedd8b46692b17e6ddc4f72321ead75a
eezhal92 <arkinthesky.69@gmail.com>
2018-12-15 19:32:50 +0800

    ubah dollar jadi rupiah

--- cart.js
+++ cart.js
@@ -17,7 +17,7 @@ function getTotalCart (lineItems) {
  * @return {string}
  */
 function formatToLocal (total) {
-  return '$ ' + total.toLocaleString()
+  return 'Rp ' + total.toLocaleString()
 }
 
 /**
